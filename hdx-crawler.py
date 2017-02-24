9c3ca11f4ba72e5007588de5786259d1ceadd567
Zibethin <nguyenththuong@gmail.com>
2017-02-24 11:51:20 +0000

    Creates working json file from csv files

--- hdx-crawler.py
+++ hdx-crawler.py
@@ -14,6 +14,7 @@ CKAN_URL = "https://data.humdata.org"
 i = 0
 #The ultimate JSON file
 JSON = {}
+hxl_header_row = 1
 
 # format for output: the dictionary key is the file name, as it should be unique:
 # { some_file_name : { 
@@ -31,27 +32,47 @@ JSON = {}
 #   name_of_file_2 : { url:"", all_hxl_tags: etc...}, 
 #   ... }
 
-def createJSON(resource):
+def populateJSON(resource, csvData):
     # escaping the "/" in the url
     #url = str(resource["url"]).replace("/", "\/")
     JSON[resource["name"]] = {}
     JSON[resource["name"]]["url"] = str(resource["url"])
+    JSON[resource["name"]]["all_hxl_tags"] = csvData
 
 def readCsv(csvLocation):
     try:
-        content = urllib.request.urlopen(csvLocation) 
-        print("content=",content,"\n\n")
+        content = urllib.request.urlopen(csvLocation)
+        #csv = csv.reader(content, delimiter=',') 
     except:
         print("-------!--- urllib not working\n")
     count = 0
 
     # to do: make count==1 dynamic (find line starting with #, if none return no valid HXL found)
+    temp_dataset = []
     for line in content:
-        if count == 1:
-            print ("line2 = ",str(line))
+        clean_line = line.decode("utf-8").replace("\n","")
+        split_line = clean_line.split(",")
+        temp_dataset.append(split_line)
+        if count == 4:
             break
         count += 1
-    
+    # create dictionary from data
+    data = {}
+    i = 0
+    try:
+        for hxl_tag in temp_dataset[1]:
+            data[str(hxl_tag)] = {}
+            data[str(hxl_tag)]["line1"] = str(temp_dataset[2][i])
+            data[str(hxl_tag)]["line2"] = str(temp_dataset[3][i])
+            data[str(hxl_tag)]["line3"] = str(temp_dataset[4][i])
+            i += 1
+    # if there is an error, return data={} so the code can continue on to other datasets
+    except:
+        data = {}
+        print("Error assigning data to json from csv")
+    print("data = ",data)
+    return data
+
 # find datasets tagged HXL
 def find_hxl_datasets(start, rows):
     """Return a page of HXL datasets."""
@@ -88,13 +109,13 @@ for package_id in package:
         # if the resource is a csv then print content
         if resource["format"] == "CSV":
             try:
-                readCsv(resource["url"])
+                csvData = readCsv(resource["url"])
             except:
                 print("Error loading csv :(\n")
             try:
-                createJSON(resource)
+                populateJSON(resource, csvData)
             except:
-                print("Error creating Json :(\n")
+                print("Error adding data to Json for file ", resource["name"], " :'( \n")
 
     print("")
     time.sleep(DELAY) # give HDX a short rest
